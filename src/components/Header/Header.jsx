a5baa026ddb78dd345f86a5f3ac20c2c1f48c142
Usubjonovshuxratbek <usubjonovshuxratbek1@gmail.com>
2023-03-03 21:24:35 +0300

    use

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../Container/Container'
+import './Header.scss'
+
+function Header() {
+  return (
+    <header>
+      <Container>
+      <h1>salom</h1>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
